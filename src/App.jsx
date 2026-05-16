632356e0b05962a5b4c90780c4edc6393792f67f
Lawrence Vincent <lawrencevincent453@gmail.com>
2026-05-16 22:40:57 +0100

    pageloader

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { PageLoader } from './components/layout/PageLoader';
+import { PageLoader } from '../src/public/components/layout/PageLoader';
 import AppRoutes from './routes/AppRoutes';
 const App = () => {
   return <PageLoader><AppRoutes /></PageLoader>
